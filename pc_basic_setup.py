ec3f6c1db8ea66c3f45763600870dcae8da18060
Luisjwhitaker <79770211+Luisjwhitaker@users.noreply.github.com>
2022-06-03 21:47:50 -0700

    adding the ability to find programs

--- pc_basic_setup.py
+++ pc_basic_setup.py
@@ -1,8 +1,17 @@
 import subprocess
+import re
+desired_computer_name = ""
 def get_current_login_user(): # gets the user name of the curren user that is logged in
-    import subprocess
-    import re
     p1 = subprocess.Popen(["powershell.exe", "whoami"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     who_am_i = p1.communicate()[0]
     user_name = re.split(r'\\r', (re.split(r'.\\\\', str(who_am_i))[1]))[0]
-    return user_name
+    print(f'{user_name} is currently logged in')
+
+def find_office_language_pack():
+    return
+
+def rename_computer():
+    return
+
+
+get_current_login_user() #testing get_current_login_user function is working.
